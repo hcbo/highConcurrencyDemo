c25d1f1e62885b91c3b2af6c8c70e4c528025978
Your Name <1298881987@qq.comjotting-webapp hcb$ git config --global user.name hcbgit config --global user.email 1298881987@qq.com>
2019-05-07 15:37:26 +0800

    第二部分

---
+++ src/main/java/com/hcb/highConcurrencyDemo/HighConcurrencyDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hcb.highConcurrencyDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighConcurrencyDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighConcurrencyDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcb/highConcurrencyDemo/HighConcurrencyDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hcb.highConcurrencyDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HighConcurrencyDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
